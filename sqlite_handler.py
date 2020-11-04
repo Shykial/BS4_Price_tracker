de2190b4b85b43867ec71078c1507d47ba85ac12
scroofer <szymon.jeziorski@interia.pl>
2020-11-04 10:09:20 +0100

    added basic SQL Injection safety

--- sqlite_handler.py
+++ sqlite_handler.py
@@ -9,19 +9,21 @@ class SQLite:
         self.cursor = self.conn.cursor()
 
     def create_table_if_not_exists(self, table_name: str):
-        self.cursor.execute(f'''CREATE TABLE IF NOT EXISTS "{table_name}" (
+        query = f'''CREATE TABLE IF NOT EXISTS "{table_name}" (
     ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL DEFAULT 1,
     price REAL NOT NULL,
-    timestamp TEXT NOT NULL
-    )''')
+    timestamp TEXT NOT NULL)'''
+        self.cursor.execute(query)
         self.conn.commit()
 
     def insert_data(self, table: str, price: float, timestamp: datetime = datetime.now()):
         self.create_table_if_not_exists(table)
         datetime_str = timestamp.strftime('%d/%m/%Y %H:%M')
-        self.cursor.execute(f'''INSERT INTO "{table}" (price, timestamp) VALUES ({price}, "{datetime_str}")''')
+        query = f'INSERT INTO "{table}" (price, timestamp) VALUES (?, ?)'
+        self.cursor.execute(query, (price, datetime_str))
         self.conn.commit()
 
     def reset_auto_increment(self, table: str):
-        self.cursor.execute(f'UPDATE SQLITE_SEQUENCE SET SEQ=0 WHERE NAME="{table}"')
+        query = 'UPDATE SQLITE_SEQUENCE SET SEQ=0 WHERE NAME=?'
+        self.cursor.execute(query, (table,))
         self.conn.commit()
