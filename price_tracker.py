13a00f085c9bae2b7de19983a1f2914e16a7dc52
scroofer <szymon.jeziorski@interia.pl>
2020-11-04 19:45:38 +0100

    multithreading on getting requests contents

--- price_tracker.py
+++ price_tracker.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from sqlite_handler import SQLite
 import time
 import re
+from concurrent.futures import ThreadPoolExecutor
 
 
 def timer(func):
@@ -15,35 +16,28 @@ def timer(func):
     return wrapper
 
 
-def get_current_price(url: str) -> float:
-    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                             'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36'}
-    r = requests.get(url, headers=headers)
-    soup = BeautifulSoup(r.content, 'html.parser')
+def get_price_from_soup(soup: BeautifulSoup) -> float:
     class_name = 'u7xnnm-4 iVazGO'
     price_text = soup.find('div', attrs={'class': class_name}).get_text()
     price = price_text.removesuffix(' zł')
     return float(price.replace(' ', '').replace(',', '.'))
 
 
-def get_name_from_url(url: str) -> str:
-    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                             'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36'}
-    r = requests.get(url, headers=headers)
-    soup = BeautifulSoup(r.content, 'html.parser')
+def get_name_from_soup(soup: BeautifulSoup) -> str:
     class_name = 'sc-1x6crnh-13 fXjZNH'
     plain_text = soup.find('div', attrs={'class': class_name}).get_text().strip()
     return re.sub(r'\s{2,}', ' ', plain_text)
 
 
-def get_laptops_dict():
-    return {
-        'https://www.x-kom.pl/p/599015-notebook-laptop-156-lenovo-legion-5-15-ryzen-5-32gb-960-win10-gtx1650.html':
-            {'name': 'Lenovo Legion 5-15 Ryzen 5/32GB/960/Win10 GTX1650',
-             'CPU': 'Ryzen 5 4600H',
-             'RAM': '32GB DDR4, 2666MHz',
-             'GPU': 'NVIDIA GeForce GTX 1650',
-             'OS_bundled': True}}
+def get_content_from_url(url: str) -> bytes:
+    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                             'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36'}
+    r = requests.get(url, headers=headers)
+    return r.content
+
+
+def get_soup_from_contents(contents: str, parse_type='html.parser') -> BeautifulSoup:
+    return BeautifulSoup(contents, parse_type)
 
 
 @timer
@@ -54,7 +48,11 @@ def main():
             'https://www.x-kom.pl/p/599019-notebook-laptop-156-lenovo-legion-5-15-ryzen-5-32gb-512-gtx1650ti-144hz.html',
             'https://www.x-kom.pl/p/599012-notebook-laptop-156-lenovo-legion-5-15-ryzen-5-32gb-480-win10-gtx1650.html']
 
-    laptops = {url: {'name': get_name_from_url(url), 'price': get_current_price(url)} for url in urls}
+    with ThreadPoolExecutor() as executor:
+        contents = executor.map(get_content_from_url, urls)
+
+    laptops = {url: {'name': get_name_from_soup(soup), 'price': get_price_from_soup(soup)}
+               for url, soup in zip(urls, map(get_soup_from_contents, contents))}
 
     for details in laptops.values():
         sqlite.insert_data(details['name'], details['price'])
