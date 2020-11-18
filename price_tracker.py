905fdcf92ac75b379293d7fad72996ebeaaecaa8
Shykial <szymon.jeziorski@interia.pl>
2020-11-18 11:23:27 +0100

    basic email handling, will need to handle scraping exceptions

--- price_tracker.py
+++ price_tracker.py
@@ -1,9 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 from sqlite_handler import SQLite
+import email_handler
 import time
 import re
 from concurrent.futures import ThreadPoolExecutor
+from email_secrets import receiver_address
 
 
 def timer(func):
@@ -35,7 +37,7 @@ def get_price_from_soup(soup: BeautifulSoup, domain: str) -> float:
 
 
 def get_name_from_soup(soup: BeautifulSoup, domain: str) -> str:
-    data = {'x-kom': {'elem_type': 'div', 'class_name': 'sc-1x6crnh-13 fXjZNH'},
+    data = {'x-kom': {'elem_type': 'h1', 'class_name': 'sc-1x6crnh-5 cYILyh'},
             'delkom': {'elem_type': 'h1', 'class_name': 'columns twelve'}}
     elem_type = data[domain]['elem_type']
     class_name = data[domain]['class_name']
@@ -61,7 +63,8 @@ def main():
     urls = ['https://www.x-kom.pl/p/602042-notebook-laptop-156-lenovo-legion-5-15-ryzen-5-32gb-512-gtx1650-120hz.html',
             'https://www.x-kom.pl/p/599019-notebook-laptop-156-lenovo-legion-5-15-ryzen-5-32gb-512-gtx1650ti-144hz.html',
             'https://www.delkom.pl/p/67116-laptop-lenovo-legion-5-15arh05-82b500ampb-16gb-ryzen-7-4800h-156fhd144hz-16gb-512ssd-gtx1650-noos.html',
-            'https://www.delkom.pl/p/67130-laptop-lenovo-legion-5-15arh05-82b500ampb-16gb-250ssd-ryzen-7-4800h-156fhd144hz-16gb-512ssd-250ssd-gtx1650-noos.html']
+            'https://www.delkom.pl/p/67130-laptop-lenovo-legion-5-15arh05-82b500ampb-16gb-250ssd-ryzen-7-4800h-156fhd144hz-16gb-512ssd-250ssd-gtx1650-noos.html',
+            'https://www.delkom.pl/p/67133-laptop-lenovo-legion-5-15arh05-82b500ampb-16gb-500ssd-ryzen-7-4800h-156fhd144hz-16gb-512ssd-500ssd-gtx1650-noos.html']
 
     with ThreadPoolExecutor() as executor:
         contents = executor.map(get_content_from_url, urls)
@@ -71,8 +74,17 @@ def main():
                for url, soup, domain in zip(urls, map(get_soup_from_contents, contents),
                                             map(get_domain_from_url, urls))}
 
-    for details in laptops.values():
+    for url, details in laptops.items():
+        new_lowest = sqlite.is_lower_than_table_min(details['name'], details['price'])
         sqlite.insert_data(details['name'], details['price'])
+
+        if new_lowest:
+            previous_lowest = new_lowest[1]
+            email_subject = f'{details["name"]} just lowered to {details["price"]}zł!'
+            email_body = f'{details["name"]} just got its price cut down from {previous_lowest} zł' \
+                         f' to {details["price"]} zł\n\nFor more details visit\n{url}'
+            email_handler.send_email(receiver_address, subject=email_subject, body=email_body)
+            print('email sent')
 
 
 if __name__ == '__main__':
